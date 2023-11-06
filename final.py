4d6ded41040a23ded25b71adf914dbb315bbd9fe
Ali Gh <work.gh.2021@gmail.com>
2023-11-06 22:03:30 +0330

    add new word to dictionary

--- final.py
+++ final.py
@@ -1,17 +1,18 @@
 from difflib import get_close_matches
 import json
 
+
 def translate(word):
     global data
     if word in data:
-        print('-' *  20)
+        print('-' * 20)
         print('Translate : ')
         print(data[word]['translate'])
-        print('-' *  20)
+        print('-' * 20)
         print('Examples : ')
         for ex in data[word]['examples']:
-            print(ex)
-        print('-' *  20)
+            print('=> ', ex)
+        print('-' * 20)
 
 
 data = json.load(open('words.json', encoding='utf-8'))
@@ -19,8 +20,10 @@ data = json.load(open('words.json', encoding='utf-8'))
 print('\n')
 print('=========== Hi ===========')
 print('\n')
-word  = input('search a word : ')
+
+word = input('search a word : ')
 alternate = get_close_matches(word, data.keys())
+
 if word in data:
     translate(word)
 elif len(alternate) > 0:
@@ -32,5 +35,25 @@ elif len(alternate) > 0:
     else:
         print('--- wrong input ---')
 else:
-    # todo : complete this part
-    print('--- oops not found ---')
+    print('--- word not found ---')
+    check = input('Should I add this word?  y/n : ')
+    examples = []
+    example = ''
+    if check in ['Y', 'y']:
+        meaning = input('Translate : ')
+        while (example != 'n'):
+            example = input('write an example (or press n) : ')
+            if (example != 'n'):
+                examples.append(example)
+        new_word = {
+            word: {
+                "translate": meaning,
+                "examples": examples
+            }
+        }
+        data.update(new_word)
+        with open('words.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        print('--- bye ---')
+    else:
+        print('--- bye ---')
